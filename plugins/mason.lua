5f5e7a6027a38d7adaa7447b904fca515297530a
Suraj N. Kurapati <sunaku@riseup.net>
2024-01-25 11:24:01 -0800

    mason: add Elixir language support

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -8,6 +8,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "lua_ls",
+        "elixirls",
       })
     end,
   },
@@ -20,6 +21,7 @@ return {
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "prettier",
         -- "stylua",
+        "elixir",
       })
     end,
   },
@@ -30,6 +32,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
         -- "python",
+        "elixir",
       })
     end,
   },
