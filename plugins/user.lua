8fff13c262695854892111cb3319b09e9b59a301
Suraj N. Kurapati <sunaku@riseup.net>
2024-01-26 17:07:25 -0800

    add abolish plugin

--- plugins/user.lua
+++ plugins/user.lua
@@ -449,6 +449,22 @@ return {
     },
   },
 
+  {
+    "tpope/vim-abolish",
+    lazy = false,
+    -- Shortcut! crc       (abolish) camelCase
+    -- Shortcut! crm       (abolish) MixedCase
+    -- Shortcut! cr_       (abolish) snake_case
+    -- Shortcut! crs       (abolish) snake_case
+    -- Shortcut! cru       (abolish) SNAKE_UPPERCASE
+    -- Shortcut! crU       (abolish) SNAKE_UPPERCASE
+    -- Shortcut! cr-       (abolish) dash-case (not usually reversible)
+    -- Shortcut! crk       (abolish) kebab-case (not usually reversible)
+    -- Shortcut! cr.       (abolish) dot.case (not usually reversible)
+    -- Shortcut! cr<space> (abolish) space case (not usually reversible)
+    -- Shortcut! crt       (abolish) Title Case (not usually reversible)
+  },
+
   ----------------------------------------------------------------------------
   -- documentation
   ----------------------------------------------------------------------------
